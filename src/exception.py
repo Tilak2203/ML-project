55ce7fec002cef86dd995afb138085200f88846f
Tilak <tilak.tjilka@gmail.com>
2025-07-25 15:37:06 -0500

    updated changes and env

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-import logging
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # Add project directory to Python path
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -12,5 +14,7 @@ def error_message_detail(error, error_detail:sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
-        self.error_message = error_message
-        self.error_detail = error_detail
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    def __str__(self):
+        return self.error_message
+    
